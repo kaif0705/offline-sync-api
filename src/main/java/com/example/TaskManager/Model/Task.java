3db14588ff3583edd02befe850b2a28bf73eabcc
kaif0705 <mohammedkaifkilledar5@gmail.com>
2025-09-04 00:30:55 +0530

    Developed Task Model

---
+++ src/main/java/com/example/TaskManager/Model/Task.java
@@ -0,0 +1,43 @@
+package com.example.TaskManager.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1024) // Define a reasonable length for the description
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
